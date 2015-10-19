9719acb6fe36a8151e291b28c2aebb8f99fb17a4
mr_prince56 <prin.baac@gmail.com>
2015-10-19 14:31:01 +0700

    Create SQLite

--- app/src/main/java/th/or/baac/klinkul/prin/baacrestaurant/MyOpenHelper.java
+++ app/src/main/java/th/or/baac/klinkul/prin/baacrestaurant/MyOpenHelper.java
@@ -1,20 +1,37 @@
 package th.or.baac.klinkul.prin.baacrestaurant;
 
 import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 
 /**
  * Created by acer on 10/19/2015.
  */
-public class MyOpenHelper {
+public class MyOpenHelper extends SQLiteOpenHelper {
 
     // Explicit
     private static final String DATABASE_NAME = "BAAC.db";
     private static final int DATABASE_VERSION = 1;
+    private static final String CREATE_USER_TABLE = "create table userTABLE (_id integer primary key, User text, Password text, Name text);";
+    private static final String CREATE_FOOD_TABLE = "create table foodTABLE (_id integer primary key, Food text, Source text, Price text);";
+
 
     public MyOpenHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
     } // Constructor
 
-    
 
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+        sqLiteDatabase.execSQL(CREATE_USER_TABLE);
+        sqLiteDatabase.execSQL(CREATE_FOOD_TABLE);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
 } // Main Class
 
---
+++ app/src/main/java/th/or/baac/klinkul/prin/baacrestaurant/UserTABLE.java
@@ -0,0 +1,23 @@
+package th.or.baac.klinkul.prin.baacrestaurant;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+/**
+ * Created by acer on 10/19/2015.
+ */
+public class UserTABLE {
+
+    // Explicite
+    private MyOpenHelper objMyOpenHelper;
+    private SQLiteDatabase writeSqLiteDatabase, readSqLiteDatabase;
+
+    public UserTABLE(Context context) {
+
+        objMyOpenHelper = new MyOpenHelper(context);
+        writeSqLiteDatabase = objMyOpenHelper.getWritableDatabase();
+        readSqLiteDatabase = objMyOpenHelper.getReadableDatabase();
+
+    } // Constructor
+
+} // Main Class
