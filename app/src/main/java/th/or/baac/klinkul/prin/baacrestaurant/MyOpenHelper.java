40f24e0aa8a9fe58a4cc3ec6c0faf5f0bbd2aecf
mr_prince56 <prin.baac@gmail.com>
2015-10-19 13:14:48 +0700

    เริ่มใช้ GitHub

---
+++ app/src/main/java/th/or/baac/klinkul/prin/baacrestaurant/MyOpenHelper.java
@@ -0,0 +1,17 @@
+package th.or.baac.klinkul.prin.baacrestaurant;
+
+import android.content.Context;
+
+/**
+ * Created by acer on 10/19/2015.
+ */
+public class MyOpenHelper {
+
+    // Explicit
+    private static final String DATABASE_NAME = "BAAC.db";
+    private static final int DATABASE_VERSION = 1;
+
+    public MyOpenHelper(Context context) {
+    } // Constructor
+
+} // Main Class
