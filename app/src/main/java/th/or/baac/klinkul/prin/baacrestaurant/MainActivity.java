69d53c5bce32f2646ebe3158fb4961f0613a6065
mr_prince56 <prin.baac@gmail.com>
2015-10-19 16:13:55 +0700

    Create SQLite

--- app/src/main/java/th/or/baac/klinkul/prin/baacrestaurant/MainActivity.java
+++ app/src/main/java/th/or/baac/klinkul/prin/baacrestaurant/MainActivity.java
@@ -7,6 +7,7 @@ public class MainActivity extends AppCompatActivity {
 
     // Explicit
     private UserTABLE objUserTABLE;
+    private FoodTABLE objFoodTABLE;
 
 
     @Override
@@ -17,10 +18,23 @@ public class MainActivity extends AppCompatActivity {
         // Create & Connect Database
         createAndConnected();
 
+        // Tester Add New Value
+        testerAdd();
+
     } // Main Method
 
+    private void testerAdd() {
+
+        objUserTABLE.addNewUser("testUser", "testPassword", "ทดสอบภาษาไทย");
+        objFoodTABLE.addNewFood("ชื่ออาหาร", "testSource", "123");
+
+    }
+
     private void createAndConnected() {
+
         objUserTABLE = new UserTABLE(this);
+        objFoodTABLE = new FoodTABLE(this);
+
     }
 
 } // Main Class
